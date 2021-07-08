0c9d5ae41d55ec40aaccd8a72e589757217e802c
mondegreen <54181197+mondegreen@users.noreply.github.com>
2021-07-08 15:15:19 -0400

    Update predictElement.pl
    Added comments describing input and output

--- predictElement.pl
+++ predictElement.pl
@@ -1,5 +1,46 @@
 use FileUtil;
 
+### this file takes a set of candidate TE junctions and tries to determine if they represent intact transposons
+###
+### The input is:
+### 1) the set of candidate TE junctions
+### 2) the transposon-masked reference genome
+### 
+### transposons that are already in the genome should be masked so if a pair of junctions adjecent on the genome in the proper orientation
+### is largely composed of N's, that identifies a known TE insertion
+###
+### conversely, if the transposon is not in the genome, then the junctions should be correctly oriented but lie very close to one another
+### in their genomic coordinates (in this case we assume <=10 bps)
+###
+### the output for unpaired TE junctions is tab-delimited and looks like:
+### 2L	309595	31	9	0
+###
+### the columns are:
+### 1) molecule
+### 2) coordinate (0 indexed interbase coordinate system)
+### 3) number of reads identified
+### 4) number of independent starts identified in the forward orientation
+### 5) number of independent starts identified in the reverse orientation
+###
+### the output for unpaired TE junctions is tab-delimited and looks like:
+### 2L	498332	236	0	29	x	7	0	x	2L	498339	30	9	0
+### 1) molecule
+### 2) coordinate (0 indexed interbase coordinate system)
+### 3) number of reads identified
+### 4) number of independent starts identified in the forward orientation
+### 5) number of independent starts identified in the reverse orientation
+### 6) spacer
+### 7) distance between second element
+### 8) number of N's between second element
+### 9) spacer
+### 10) molecule
+### 11) coordinate (0 indexed interbase coordinate system)
+### 12) number of reads identified
+### 13) number of independent starts identified in the forward orientation
+### 14) number of independent starts identified in the reverse orientation
+
+
+
 my $file = shift;
 my $fasta = shift;
 
